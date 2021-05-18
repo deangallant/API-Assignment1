855244b0c7f4a508f5862cf901388b1d5a78e058
deangallant <deangallant@student.mitt.ca>
2021-05-18 14:19:09 -0500

    add data to table from search results

--- app.js
+++ app.js
@@ -10,18 +10,14 @@
 // 3.  Stop Schedule endpoint
 // a. from 2A I can get the Stops "key" to find the next 2 bus arrival times by querying the STOP SCHEDULE endpoint.
 
-// let mychoice = '';
-// const streetsUrl = `https://api.winnipegtransit.com/v3/streets.json?api-key=yXm1l1BHCfXG4tZHyorJ&name=${mychoice}`;
 
 const streetQueryList = document.querySelector('.streets')
 const search = document.querySelector('.search')
+const table = document.querySelector('.table')
 
 const streets = async (streetsUrl) => {
-  
   const request = await fetch(streetsUrl);
   const data = await request.json();
-  
-  
   streetListRender(data.streets)
 }
 
@@ -29,31 +25,38 @@ const stopLocations = async (streetKey) => {
   //const streetKey = streetNum[0].key
   console.log(streetKey)
   const stopLocations = await fetch(`https://api.winnipegtransit.com/v3/stops.json?api-key=yXm1l1BHCfXG4tZHyorJ&street=${streetKey}`);
-  const individualStopData = await stopLocations.json();
-  console.log(individualStopData)
-  stopListRender(individualStopData)
-  return individualStopData;
+  const individualStopLocations = await stopLocations.json();
+  console.log(individualStopLocations)
+  //stopListRender(individualStopLocations)
+  //loop over indivdualStopData, get stop keys and render?
+  return stopSchedules(individualStopLocations)
 }
 
-const stopSchedules = async (stopKey) => {
+const stopSchedules = async (individualStopSchedules) => {
   //const stopNum = stopKey.stops[0].key;
   //console.log(`stop number ${stopNum}`)
-  const stopScheduleRequest = await fetch(`https://api.winnipegtransit.com/v3/stops/${stopNum}/schedule.json?api-key=yXm1l1BHCfXG4tZHyorJ`)
-  const stopScheduleData = await stopScheduleRequest.json()
-  const nextBus = stopScheduleData
-  
-  //console.log(nextBus)
-  
+  console.log(typeof individualStopSchedules)
+  console.log(individualStopSchedules)
+  let stopNum = []
+  individualStopSchedules.stops.forEach(ele => {
+    
+    stopNum.push(ele.key)
+    
+  })
+  //console.log(stopNum)
+  stopListRender(stopNum)
+
 }
 
 const streetListRender = (streets) => {
   console.log(streets)
+  console.log(typeof streets)
   streets.forEach(street => {
     streetQueryList.insertAdjacentHTML('afterbegin', `
     <a href="#" class='list' data-street-key="${street.key}">${street.name}</a>
     `)
-    
   });
+
   const atags = document.querySelectorAll('.list')
   atags.forEach(ele => {
     ele.addEventListener('click', (e) => {
@@ -64,11 +67,31 @@ const streetListRender = (streets) => {
 }
 
 const stopListRender = (stopLocations) => {
+  console.log(stopLocations)
+  console.log(typeof stopLocations)
+  stopLocations.forEach(async stopNum => {
+    
+    const stopScheduleRequest = await fetch(`https://api.winnipegtransit.com/v3/stops/${stopNum}/schedule.json?api-key=yXm1l1BHCfXG4tZHyorJ`)
+    const stopScheduleData = await stopScheduleRequest.json()
+    console.log(stopScheduleData['stop-schedule']['route-schedules'][0]['scheduled-stops'][0]['times']['arrival']['estimated'])
+    
+   
 
+
+    table.insertAdjacentHTML('afterbegin', `
+    <tr>
+            <td>${stopScheduleData['stop-schedule']['stop']['name']}</td>
+            <td>${stopScheduleData['stop-schedule']['stop']["cross-street"].name}</td>
+            <td>${stopScheduleData['stop-schedule']['stop']['direction']}</td>
+            <td>${stopScheduleData['stop-schedule']['route-schedules'][0]['route']['number']}</td>
+            <td>${stopScheduleData['stop-schedule']['route-schedules'][0]['scheduled-stops'][0]['times']['arrival']['estimated']}</td>
+          </tr>
+    `)
+  })
 }
 
 search.addEventListener('keydown', (e) => {
-  //console.log(e.target)
+  
   if (e.code === 'Enter' || e.code === 'NumpadEnter') {
     if (e.target.value !== '') {
     let mychoice = e.target.value;
@@ -79,16 +102,6 @@ search.addEventListener('keydown', (e) => {
 }
 })
 
-//<a href="#" data-street-key="4499">Kenaston Common Drive</a>
-// e.target.value = ''
-// e.preventDefault()
+// const stopScheduleRequest = await fetch(`https://api.winnipegtransit.com/v3/stops/${stopNum}/schedule.json?api-key=yXm1l1BHCfXG4tZHyorJ`)
+//     const stopScheduleData = await stopScheduleRequest.json()
 
-//event.target.nodeName === 'INPUT'
-
-// <tr>
-//             <td>Kenaston Boulevard</td>
-//             <td>Rothwell Road</td>
-//             <td>Northbound</td>
-//             <td>74</td>
-//             <td>02:25 PM</td>
-//           </tr>
